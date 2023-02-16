4d0114b213fe7ad26c0efbc2fbd828b023ec324f
BidanGitau <bydangitau@gmail.com>
2023-02-16 16:46:20 +0300

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,12 @@ Rails.application.configure do
   #       :authentication => :plain,
   #       :enable_starttls_auto => true
   # }
+  ActionMailer::Base.smtp_settings = {
+    domain: 'https://tenancy-production.up.railway.app',
+    address:        "smtp.sendgrid.net",
+    port:            587,
+    authentication: :plain,
+    user_name:      'apikey',
+    password:       Rails.credentials.dig(:sendgrid,:access_key)
+}
 end
