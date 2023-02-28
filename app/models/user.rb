3b807e2154b66d246ac697974563a65268b23dd2
BidanGitau <bydangitau@gmail.com>
2023-02-28 18:18:38 +0300

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,:confirmable,
-         :recoverable, :rememberable, :validatable,:invitable
-         has_one :account
-        
-         before_validation :set_account
+  devise :invitable,
+         :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :invitable
+  has_one :account
 
-         def set_account
-           self.build_account
-         end
+  before_validation :set_account
 
-         def send_invitation_to(tenant)
-          tenant.update(password: SecureRandom.hex(8))
-           TenantMailer.invitation(tenant, self, tenant.password).deliver_later
-        end
+  def set_account
+    self.build_account
+  end
+
+  def send_invitation_to(tenant)
+    tenant.update(password: SecureRandom.hex(8))
+    TenantMailer.invitation(tenant, self, tenant.password).deliver_later
+  end
 end
