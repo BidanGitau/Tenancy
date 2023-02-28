3092af48210e39789cd3da603338584682527fce
BidanGitau <bydangitau@gmail.com>
2023-02-28 17:44:26 +0300

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: "tenancy-production.up.railway.app",
   }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.require_master_key = true
