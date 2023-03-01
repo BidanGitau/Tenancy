7c3258d59d4c4dd7792a62dc7c0dd7f7df836b9e
BidanGitau <bydangitau@gmail.com>
2023-03-01 08:53:18 +0300

    user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
-    private
-  
-    def sign_up_params
-      params.require(:user).permit(:firstname,:lastname, :email, :password, :password_confirmation)
-    end
-  
-    def user_update_params
-      params.require(:user).permit(:firstname,:lastname, :email, :password, :password_confirmation, :current_password)
-    end
+  private
+
+  def after_sign_up_path_for(resource)
+    dashboard_path
   end
-  
+  def sign_up_params
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email,
+      :password,
+      :password_confirmation,
+    )
+  end
+
+  def user_update_params
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email,
+      :password,
+      :password_confirmation,
+      :current_password,
+    )
+  end
+end
